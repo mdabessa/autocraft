8af8200f14cf8e56382d79ba1b784408558e57e2
Matheus Bessa <mdabessa@gmail.com>
2023-03-13 01:13:33 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 Libs = require('libs/init')
 
+log('Start!')
+
 Home.resetHome()
 if Home.getHome() == nil then
     Home.createHome()
 end
 
 Crafting.craft('minecraft:iron_sword', 1)
+Inventory.sortHotbar()
 
 log('Done!')
