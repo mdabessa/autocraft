ddf8cdee0dc5baf7f2340ce39b04b5298ddc1071
Matheus Bessa <mdabessa@gmail.com>
2023-03-19 01:48:23 -0300

    fix: when craft crafting_table, return to recalculate the recipe

--- libs/crafting.lua
+++ libs/crafting.lua
@@ -124,8 +124,8 @@ crafting.craftingTable = function(recipe)
     if crafting_table == nil then
         Home.buildWorkbench()
         crafting_table = World.searchBlock('minecraft:crafting_table', 2)
-
         if crafting_table == nil then error('No crafting table found') end
+        return
     end
 
 
