562ba895e88d4dc28955f5e61fb96018e77cd902
Matheus Bessa <mdabessa@gmail.com>
2023-04-04 23:20:04 -0300

    Execute commands in chat and save non-command events to database

--- macros/event_chat.lua
+++ macros/event_chat.lua
@@ -1,6 +1,13 @@
 Libs = require('libs/init')
 local args = {...}
 
-Database.add_event(args[4])
+local chat = Str.split(args[4], '>')
+if string.sub(chat[2], 2, 2) == '!' then -- 2 because of the space
+    local cmd = string.sub(chat[2], 3, -1)
+    Command.execute(cmd)
+
+else
+    Database.add_event(args[4])
+end
 
 return args[4]
