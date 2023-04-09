4c3cff96ec3089c950066250fce908a198709942
Matheus Bessa <mdabessa@gmail.com>
2023-04-09 00:14:28 -0300

    Update event_chat.lua

--- macros/event_chat.lua
+++ macros/event_chat.lua
@@ -1,13 +1,14 @@
 Libs = require('libs/init')
 local args = {...}
 
-local chat = Str.split(args[4], '>')
-if string.sub(chat[2], 2, 2) == '!' then -- 2 because of the space
-    local cmd = string.sub(chat[2], 3, -1)
-    Command.execute(cmd)
-
-else
-    Database.add_event(args[4])
-end
+pcall(function()
+    local chat = Str.split(args[4], '>')
+    if string.sub(chat[2], 2, 2) == '!' then -- 2 because of the space
+        local cmd = string.sub(chat[2], 3, -1)
+        Command.execute(cmd)
+    else
+        Database.add_event(args[4])
+    end
+end)
 
 return args[4]
