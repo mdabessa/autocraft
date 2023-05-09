3ba0694ed9a58ce2ee615a55ddeaa1c4a0fa03c6
Matheus Bessa <mdabessa@gmail.com>
2023-05-09 00:12:43 -0300

    Update main.lua

--- macros/main.lua
+++ macros/main.lua
@@ -1,22 +1,26 @@
 Libs = require('libs/init')
 
-Home.resetHome()
-if Home.getHome() == nil then
-    Home.createHome()
-end
-
-local counter = 0
-while true do
-    if #Command.threads==0 then
-        counter = counter + 1
-        if counter == 5 then
-            counter = 0
-            log('Crafting diamond')
-            Command.execute('craft minecraft:diamond')
-        end
-    else
-        counter = 0
+local function main()
+    Home.resetHome()
+    if Home.getHome() == nil then
+        Home.createHome()
     end
 
-    sleep(1000)
+    local counter = 0
+    while true do
+        if #Command.threads==0 then
+            counter = counter + 1
+            if counter == 5 then
+                counter = 0
+                log('Crafting diamond')
+                Command.execute('craft minecraft:diamond')
+            end
+        else
+            counter = 0
+        end
+
+        sleep(1000)
+    end
 end
+
+pcall(main)
