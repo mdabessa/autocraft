2bc3f5fc24f7fb81233d4b9f2f0fc944d0ab9840
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2023-07-11 23:46:54 -0300

    Implement "str.errorResume" function to enhance error message parsing and extraction.

--- macros/libs/utils/str.lua
+++ macros/libs/utils/str.lua
@@ -9,4 +9,19 @@ str.split = function(_str, sep)
     return fields
 end
 
+str.errorResume = function(err)
+    local msg = str.split(err, '\n')
+    msg = msg[1]
+    msg = str.split(msg, ':')
+    local res = ''
+    for i = 3, #msg do
+        res = res .. msg[i] .. ':'
+    end
+    res = res:sub(1, -2)
+
+    local first_space = res:find(' ')
+    res = res:sub(first_space + 1, -1)
+    return res
+end
+
 return str
