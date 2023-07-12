f5d7c58ee6dcdcab31fecab318aa255f938f44fa
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2023-07-11 23:14:17 -0300

    Update event_chat.lua

--- macros/event_chat.lua
+++ macros/event_chat.lua
@@ -1,13 +1,23 @@
 Libs = require('libs/init')
 local args = {...}
 
+
+local function callback(status, err)
+    if not status then
+        log(err)
+    end
+end
+
+
 pcall(function()
     local chat = Str.split(args[4], '>')
+    local user = string.sub(chat[1], 2, -1)
     if string.sub(chat[2], 2, 2) == '!' then -- 2 because of the space
         local cmd = string.sub(chat[2], 3, -1)
-        Command.execute(cmd)
-    else
+        Command.execute(cmd, callback)
+    elseif user ~= 'Ailicia' then
         Database.add_event(args[4])
+        Logger.log('Added event: ' .. args[4])
     end
 end)
 
