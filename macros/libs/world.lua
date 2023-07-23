2ec6714a3cc1acf1faa812e5efc6827b1b9bdd8f
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2023-07-23 04:02:37 -0300

    Refactor the function searchInRadius to flat style
    because of the change from version 1.12.2 to 1.15.2, coroutine functions don't work due to a mod bug, so the function was refactored to not use coroutines.

--- macros/libs/world.lua
+++ macros/libs/world.lua
@@ -2,18 +2,17 @@ local world = {}
 
 world.searchStructure = function(func, timeout, iterator, shape, step)
     timeout = timeout or 5
-    local start = os.clock()
+    local start = os.time()
     local player = getPlayer()
 
     local x = math.floor(player.pos[1])
     local y = math.floor(player.pos[2])
     local z = math.floor(player.pos[3])
 
-    iterator = iterator or world.searchInRadius
-    iterator = coroutine.wrap(iterator)
+    iterator = iterator or world.searchInRadius()
     while true do
-        if os.clock() - start > timeout then return nil end
-        local pos = iterator(shape, step)
+        if os.time() - start > timeout then return nil end
+        local pos = iterator()
         if pos == nil then return nil end
         pos = {pos[1] + x, pos[2] + y, pos[3] + z}
         local result = func(pos)
@@ -41,35 +40,93 @@ world.searchInRadius = function(shape, step)
     if type(shape) == 'number' then shape = {shape, shape, shape} end
     if type(step) == 'number' then step = {step, step, step} end
 
-    local m = math.max(shape[1], shape[2], shape[3])
+    local max_layer = math.max(shape[1], shape[2], shape[3])
 
-    for i = 1, m do
-        local ranges = {
-            Calc.range(-i, i),
-            Calc.range(-i, i),
-            Calc.range(-i, i)
-        }
+    local state = {}
+    local loops = {
+        {start = 1, stop = max_layer, step = 1, index = 1},
+    }
+    local function next()
+        while true do
+            if #loops == 0 then return nil end
 
-        for axis = 1, 3 do
-            ranges[axis] = {-i, i}
-            for _, x in ipairs(ranges[1]) do
-                if x > shape[1] or x < -shape[1] then goto continue1 end
-                for _, y in ipairs(ranges[2]) do
-                    if y > shape[2] or y < -shape[2] then goto continue2 end
-                    for _, z in ipairs(ranges[3]) do
-                        if z > shape[3] or z < -shape[3] then goto continue3 end
-                        if x % step[1] == 0 and y % step[2] == 0 and z % step[3] == 0 then
-                            coroutine.yield({x, y, z})
-                        end
-                        ::continue3::
-                    end
-                    ::continue2::
-                end
-                ::continue1::
+            -- main
+            if #loops == 1 then
+                local axis = {
+                    start = 1,
+                    stop = 3,
+                    step = 1,
+                    index = 1,
+                }
+                table.insert(loops, axis)
+
+                local loop = loops[1]
+                -- reset axis iterating
+                state["ranges"] = {{start = -loop.index, stop=loop.index, step=1, index= -loop.index},
+                                    {start = -loop.index, stop=loop.index, step=1, index= -loop.index},
+                                    {start = -loop.index, stop=loop.index, step=1, index= -loop.index}}
+
+                state["border"] = loop.index
             end
-            ranges[axis] = Calc.range(-i+1, i-1) -- remove duplicates corners
+
+            if #loops == 2 then
+                local loop = loops[2]
+                -- lock axis iterating only in the start and end
+                for i=1, 3 do
+                    if i == loop.index then
+                        state["ranges"][i].step = (state["ranges"][i].stop * 2)
+                    elseif i < loop.index then
+                        state["ranges"][i].step = 1
+                        -- skip edges to avoid duplicates
+                        state["ranges"][i].start = (state["border"] - 1) * -1
+                        state["ranges"][i].stop = state["border"] - 1
+                        state["ranges"][i].index = state["ranges"][i].start
+                    else
+                        state["ranges"][i].step = 1
+                    end
+                end
+                -- create x axis iteration
+                table.insert(loops, Table.copy(state["ranges"][1]))
+            end
+
+            if #loops == 3 then
+                -- create y axis iteration
+                table.insert(loops, Table.copy(state["ranges"][2]))
+            end
+
+            if #loops == 4 then
+                -- create z axis iteration
+                table.insert(loops, Table.copy(state["ranges"][3]))
+            end
+
+            -- get current position
+            local result = {
+                loops[3].index,
+                loops[4].index,
+                loops[5].index,
+            }
+
+            -- increment and update loops
+            loops[#loops].index = loops[#loops].index + loops[#loops].step
+            for i=#loops, 1, -1 do
+                if loops[i].index > loops[i].stop then
+                    if i ~= 1 then
+                        loops[i-1].index = loops[i-1].index + loops[i-1].step
+                    end
+                    table.remove(loops, i)
+                end
+            end
+
+
+            -- validate position
+            if result[1] % step[1] == 0 and result[2] % step[2] == 0 and result[3] % step[3] == 0 then
+                if math.abs(result[1]) <= shape[1] and math.abs(result[2]) <= shape[2] and math.abs(result[3]) <= shape[3] then
+                    return result
+                end
+            end
         end
     end
+    return next
 end
 
 return world
