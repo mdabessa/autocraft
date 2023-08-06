f38f5238f78bd1248d9e9f8c1972b4e54b8d8d8f
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2023-08-05 21:49:46 -0300

    Write log message with timestamp to file

--- macros/libs/utils/logger.lua
+++ macros/libs/utils/logger.lua
@@ -2,6 +2,7 @@ local logger = {}
 
 logger.verbose = true
 logger.callback = nil
+logger.filepath = '../../logs/' .. Str.generateFileName() .. '.log'
 
 logger.log = function (msg)
     if logger.verbose then
@@ -9,6 +10,14 @@ logger.log = function (msg)
     end
     if logger.callback ~= nil then
         logger.callback(msg)
+    end
+
+    if logger.filepath ~= nil then
+        local file = io.open(logger.filepath, 'a')
+        local date = os.date("%d-%m-%Y %H:%M:%S %z")
+
+        file:write("[" .. date .. "] " .. msg .. "\n")
+        file:close()
     end
 end
 
