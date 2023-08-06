1467ee5755c436270e24dbba662b068eb56dabf1
Matheus Bessa <76802830+mdabessa@users.noreply.github.com>
2023-08-06 01:18:49 -0300

    Add event handlers for game events

--- macros/event_anything.lua
+++ macros/event_anything.lua
@@ -1,14 +1,31 @@
 Libs = require('libs/init')
 local args = {...}
 
-local whitelist = {
-    Death = true,
-    PlayerJoin = true,
-    PlayerLeave = true,
-}
+local events = {}
 
-if whitelist[args[2]] then
-    Database.add_event(args[1])
-    Logger.log('Added event: ' .. args[1])
+events.Death = function (arg)
+    return "You died"
+end
+
+events.PlayerJoin = function (arg)
+    return "The player " .. arg[3] .. " joined the game"
+end
+
+events.PlayerLeave = function (arg)
+    return "The player " .. arg[3] .. " left the game"
+end
+
+events.Weather = function (arg)
+    return "The weather changed to " .. arg[3]
+end
+
+events.Respawn = function (arg)
+    return "You respawned"
+end
+
+if events[args[2]] then
+    local message = events[args[2]](args)
+    Logger.log(message)
+    Database.add_event(message)
 end
 
